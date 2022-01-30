5c08b5a713ee103062d9589f0cc8d8122f6b4035
stuti <stutimegha2019@gmail.com>
2022-01-30 17:26:01 +0530

    Sliding Window

---
+++ Pattern_Sliding_Window/SmallestSubarrayGivenSum.java
@@ -0,0 +1,8 @@
+
+//Problem Statement #
+//Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’
+
+package Pattern_Sliding_Window;
+public class SmallestSubarrayGivenSum {
+
+}
