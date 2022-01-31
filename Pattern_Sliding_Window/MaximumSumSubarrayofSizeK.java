567dfe46579c44a9beccb12fdbe7cad26975259e
stuti <stutimegha2019@gmail.com>
2022-01-31 22:50:46 +0530

    test

--- Pattern_Sliding_Window/MaximumSumSubarrayofSizeK.java
+++ Pattern_Sliding_Window/MaximumSumSubarrayofSizeK.java
@@ -4,29 +4,50 @@ package Pattern_Sliding_Window;
 
 public class MaximumSumSubarrayofSizeK {
     public static int SubArray(int[] arr,int k) {
+        if(arr.length==0 || k <=0 || k>arr.length ){
+            return 0;
+        }
+
+      //////Brute Force /////
         int sum=0;
-        int start=0;
         int max_sum=0;
 
-        for(int i=0;i<k;i++){
-            sum=sum+arr[i];
-        }
-        max_sum = sum;
-        for(int j=k;j<arr.length;j++){
-            sum = sum + arr[j] - arr[start];
-            start++;
-            if(sum>max_sum){
-                max_sum = sum;
+        for(int i=0;i<arr.length-k;i++){
+           sum=0;
+            for(int j=i;j<i+k;j++){
+                sum = sum+arr[j];
             }
+            max_sum=Math.max(max_sum,sum);
         }
         return max_sum;
+
+        ///// method 2////////
+
+//        int sum=0;
+//        int start=0;
+//        int max_sum=0;
+//        for(int i=0;i<k;i++){
+//            sum=sum+arr[i];
+//        }
+//        max_sum = sum;//
+//        for(int j=k;j<arr.length;j++){
+//            sum = sum + arr[j] - arr[start];
+//            start++;
+//            if(sum>max_sum){ //
+//                max_sum = sum;
+//            }
+//        }
+//        return max_sum; //
     }
     public static void main(String[] args){
         int [] arr={2, 1, 5, 1, 3, 2};
         System.out.println(SubArray(arr,3));
-        System.out.println(SubArray(arr,2));
+        System.out.println(SubArray(arr,2)); //
         System.out.println(SubArray(new int [] {2, 3, 4, 1, 5} , 2));
-        System.out.println(SubArray(new int [] {2, 3, 4, 1, 5} , 3));
+        System.out.println(SubArray(new int [] {2, 3, 4, 1, 5,3} , 3));
     }
 }
-//
+// return 0
+//brute force
+
+
--- Pattern_Sliding_Window/SmallestSubarrayGivenSum.java
+++ Pattern_Sliding_Window/SmallestSubarrayGivenSum.java
@@ -4,5 +4,32 @@
 
 package Pattern_Sliding_Window;
 public class SmallestSubarrayGivenSum {
+    public static int SmallestSubarray(int[] arr, int k){
+       if(arr.length ==0 ){
+        return 0;
+     }
+        int minLength = Integer.MAX_VALUE;
+        int sum = 0;
 
+        for(int i = 0; i < arr.length; i++) {
+            sum = 0;
+
+            for(int j = i; j < arr.length; j++) {
+
+                sum = sum + arr[j];
+
+                if(sum >= k ) {
+                    minLength = Math.min(minLength, (j-i)+1);
+                }
+            }
+        }
+        return minLength;
+    }
+    public static void main(String[] args){
+        int [] arr={2, 1, 5, 1, 3, 2};
+        System.out.println(SmallestSubarray(arr,3));
+        System.out.println(SmallestSubarray(arr,8)); //
+        System.out.println(SmallestSubarray(new int [] {2, 3, 4, 1, 5} , 4));
+        System.out.println(SmallestSubarray(new int [] {2, 3, 4, 1, 5,3} , 3));
+    }
 }
