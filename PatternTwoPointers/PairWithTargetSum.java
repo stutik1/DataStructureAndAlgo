0cdee0e10fe91456527bae4355e3df038ff80fe3
stuti <stutimegha2019@gmail.com>
2022-02-18 00:16:33 +0530

    Solution

--- PatternTwoPointers/PairWithTargetSum.java
+++ PatternTwoPointers/PairWithTargetSum.java
@@ -1,8 +1,11 @@
 package PatternTwoPointers;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class PairWithTargetSum {
     public static void targetSum(int arr[],int target) {
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length;i++){
             for(int j=i+1;j< arr.length;j++){
                 int sum = arr[i]+arr[j];
                 if(sum == target ){
@@ -11,9 +14,27 @@ public class PairWithTargetSum {
             }
         }
     }
+
+    public static void targetSumByTwoPointers(int arr[], int target){
+        Arrays.sort(arr);
+        int low = 0;
+        int high = arr.length-1;
+        while (low<high){
+            if(arr[low] + arr[high] == target){
+                System.out.println("pair found " + arr[low] + "," + arr[high]);
+            }
+             if(arr[low] + arr[high] < target){
+                low++;
+            }
+            else  {
+                high--;
+            }
+        }
+    }
     public static void main(String[] args) {
-        int arr[] = {1, 3, 5, 2};
+        int arr[] = {1,2, 3,4,5};
         int target = 5;
-        targetSum(arr,target);
+       //targetSum(arr,target);
+        targetSumByTwoPointers(arr,target);
     }
 }
