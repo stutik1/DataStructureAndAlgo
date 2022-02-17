29e98259b57aecaac4ec9f7508137a003c9f5c72
stuti <stutimegha2019@gmail.com>
2022-02-18 00:52:28 +0530

    Solution

--- PatternTwoPointers/PrintAllPairs.java
+++ PatternTwoPointers/PrintAllPairs.java
@@ -9,8 +9,16 @@ public class PrintAllPairs {
         }
     }
 
+        public static void PrintPair1(int arr[]) {
+        for (int i=0;i< arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                System.out.println(arr[i] + "," + arr[j]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = {1,2,3};
+        int arr[] = {1,2,3,4,5};
         PrintPair(arr);
 
     }
