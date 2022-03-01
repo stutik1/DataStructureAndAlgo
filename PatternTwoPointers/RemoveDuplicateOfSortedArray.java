fefb85f95fab0dafb75a99ebc229eb2f345ad1e2
stuti <stutimegha2019@gmail.com>
2022-03-01 21:31:19 +0530

    Solution

---
+++ PatternTwoPointers/RemoveDuplicateOfSortedArray.java
@@ -0,0 +1,41 @@
+package PatternTwoPointers;
+
+public class RemoveDuplicateOfSortedArray {
+    public static void Duplicate(int arr[]) {
+        int temp[] = new int[arr.length];
+        int j=0;
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i] != arr[i+1]){
+                temp[j] = arr[i];
+                j++;
+            }
+        }
+            temp[j] = arr[arr.length-1];
+            for (int i=0;i<temp.length;i++){
+                System.out.print(temp[i] + " ");
+            }
+    }
+
+    public static void Duplicate1(int arr[]) {
+      //  int temp[] = new int[arr.length];
+        int j=0;
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i] != arr[i+1]){
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        arr[j] = arr[arr.length-1];
+        for (int i=0;i< j+1;i++){  // print all j elements
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,3,4};
+        //Duplicate(arr);
+         Duplicate1(arr);
+
+    }
+}
