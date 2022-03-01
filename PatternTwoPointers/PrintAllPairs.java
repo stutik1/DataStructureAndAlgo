7f8ad7cebd9127d522a9d3bb6df08f18812af2ad
stuti <stutimegha2019@gmail.com>
2022-03-01 21:36:53 +0530

    Solution

--- PatternTwoPointers/PrintAllPairs.java
+++ PatternTwoPointers/PrintAllPairs.java
@@ -4,7 +4,7 @@ public class PrintAllPairs {
     public static void PrintPair(int arr[]) {
         for (int i=0;i< arr.length;i++){
             for (int j=0;j<arr.length;j++){
-                System.out.println(arr[i] + "," + arr[j]);
+                System.out.print("(" + arr[i] + "," + arr[j]+ ")");
             }
         }
     }
@@ -12,14 +12,15 @@ public class PrintAllPairs {
         public static void PrintPair1(int arr[]) {
         for (int i=0;i< arr.length;i++){
             for (int j=i+1;j<arr.length;j++){
-                System.out.println(arr[i] + "," + arr[j]);
+                System.out.println("(" + arr[i] + "," + arr[j]+ ")");
             }
         }
     }
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
-        PrintPair(arr);
+        //PrintPair(arr);
+        PrintPair1(arr);
 
     }
 }
