c7d18f9ac550f7280c838652face4e84e1b1dd4d
stuti <stutimegha2019@gmail.com>
2022-03-08 16:19:34 +0530

    Practice

--- PatternFastSlowPointers/LengthOfLinkList.java
+++ PatternFastSlowPointers/LengthOfLinkList.java
@@ -1,9 +1,14 @@
 package PatternFastSlowPointers;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 class Node {
     int data ;
     Node next = null;
-    Node(int value){
-
+    Node(int num){
+        data = num;
+        next=null;
     }
 }
 
@@ -20,12 +25,61 @@ public class LengthOfLinkList {
         }
         return count;
     }
+
+    public static int findLengthByHashSet(Node head){
+        Node elements = head;
+
+        HashSet<Node> set = new HashSet<>();
+        int temp =0;
+        while (elements!=null){
+            if (set.contains(elements)){
+                break;
+            }else {
+                set.add(elements);
+                elements = elements.next;
+                temp++;
+            }
+        }
+        Node containElements = head;
+        int temp2 = 0;
+        while ((containElements!=elements)){
+            temp2++;
+            containElements=containElements.next;
+        }
+        return temp-temp2+1;
+    }
+
+    public static int findLengthByHashMap(Node head){
+        Node element = head;
+        int position = 0;
+        HashMap<Node,Integer> hm = new HashMap<>();
+        while (element!=null) {
+            if (!hm.containsKey(element)){
+                hm.put(element,position);
+                position++;
+            } else{
+
+                return (position-hm.get(element)+1);
+
+            }
+            element=element.next;
+        }
+        return 0;
+    }
     public static void main(String[] args) {
         Node head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
         head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(7);
         System.out.println(findLength(head));
+
+        head.next.next.next.next.next.next= head.next.next;
+        System.out.println(findLengthByHashSet(head));
+
+        head.next.next.next.next.next.next= head.next.next;
+        System.out.println(findLengthByHashMap(head));
     }
 }
